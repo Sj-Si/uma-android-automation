745fb56b9700388223d676488a7cf8b6e29a35a7
Sj-Si <sjw.jetty@gmail.com>
2025-09-29 11:14:49 -0400

    Add additional regions to Screen object.

--- app/src/main/java/com/steve1316/uma_android_automation/utils/ScreenUtils.kt
+++ app/src/main/java/com/steve1316/uma_android_automation/utils/ScreenUtils.kt
@@ -20,17 +20,37 @@ object Screen {
             return MediaProjectionService.region.displayHeight
         }
 
+    // SCREEN REGIONS
+
     // TODO: Separate tablet landscape scale to non-splitscreen and splitscreen scales.
+
+    var FULL: IntArray = intArrayOf(0, 0, 0, 0)
+
+    // Top part of screen containing title bars and whatnot.
+    var TOP: IntArray = intArrayOf(0, 0, 0, 0)
+        get() {
+            return intArrayOf(0, 0, WIDTH, HEIGHT / 6)
+        }
+
     var TOP_HALF: IntArray = intArrayOf(0, 0, 0, 0)
         get() {
             return intArrayOf(0, 0, WIDTH, HEIGHT / 2)
         }
+
+    // 50% of total height of display, centered vertically.
     var MIDDLE: IntArray = intArrayOf(0, 0, 0, 0)
         get() {
             return intArrayOf(0, HEIGHT / 4, WIDTH, HEIGHT / 2)
         }
+
     var BOTTOM_HALF: IntArray = intArrayOf(0, 0, 0, 0)
         get() {
             return intArrayOf(0, HEIGHT / 2, WIDTH, HEIGHT / 2)
+        }
+
+    // Bottom part of screen containing menu bars and whatnot.
+    var BOTTOM: IntArray = intArrayOf(0, 0, 0, 0)
+        get() {
+            return intArrayOf(0, HEIGHT / 6, WIDTH, HEIGHT / 6)
         }
 }
