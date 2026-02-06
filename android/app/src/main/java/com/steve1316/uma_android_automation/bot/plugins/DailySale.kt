2a964a00fd50fcbeb66e2f0ae91f5603869a503c
Sj-Si <sjw.jetty@gmail.com>
2026-02-05 21:37:43 -0500

    Fix various bugs.

--- android/app/src/main/java/com/steve1316/uma_android_automation/bot/plugins/DailySale.kt
+++ android/app/src/main/java/com/steve1316/uma_android_automation/bot/plugins/DailySale.kt
@@ -1,6 +1,7 @@
 package com.steve1316.uma_android_automation.bot.plugins
 
 import android.graphics.Bitmap
+import org.opencv.core.Point
 
 import com.steve1316.automation_library.utils.MessageLog
 import com.steve1316.automation_library.data.SharedData
@@ -12,6 +13,7 @@ import com.steve1316.uma_android_automation.bot.plugins.Plugin
 import com.steve1316.uma_android_automation.bot.plugins.DialogHandlerCallback
 import com.steve1316.uma_android_automation.bot.plugins.DialogHandlerResult
 import com.steve1316.uma_android_automation.utils.ScrollList
+import com.steve1316.uma_android_automation.utils.types.BoundingBox
 
 import com.steve1316.uma_android_automation.components.ComponentInterface
 import com.steve1316.uma_android_automation.components.DialogInterface
@@ -50,6 +52,8 @@ class DailySale(
         .mapNotNull { it -> SaleItem.fromName(it) }
     override val bIsEnabled: Boolean = super.bIsEnabled && saleItemsToBuy.isNotEmpty()
 
+    private var bSaleExpired: Boolean = false
+
     override fun handleDialogs(dialog: DialogInterface?): DialogHandlerResult {
         val result: DialogHandlerResult = super.handleDialogs(dialog)
         if (result !is DialogHandlerResult.Unhandled) {
@@ -60,7 +64,10 @@ class DailySale(
             "confirm_exchange" -> result.dialog.ok(game.imageUtils)
             "end_sale_confirmation" -> result.dialog.ok(game.imageUtils)
             "exchange_complete" -> result.dialog.close(game.imageUtils)
-            "return_to_shops" -> result.dialog.close(game.imageUtils)
+            "return_to_shops" -> {
+                bSaleExpired = true
+                result.dialog.close(game.imageUtils)
+            }
             else -> return DialogHandlerResult.Unhandled(result.dialog)
         }
         game.wait(0.5, skipWaitingForLoading = true)
@@ -107,6 +114,100 @@ class DailySale(
         return waitForPage(PageDailySale)
     }
 
+    private fun onListEntry(
+        scrollList: ScrollList,
+        index: Int,
+        component: ComponentInterface,
+        point: Point,
+        bitmap: Bitmap,
+    ): Boolean {
+        var prevNames: Set<String> = setOf()
+
+        var numToHandle: Int = listOf<Boolean>(
+            saleItemsToBuy.contains(SaleItem.STAR_PIECE),
+            saleItemsToBuy.contains(SaleItem.STAR_PIECE),
+            saleItemsToBuy.contains(SaleItem.ALARM_CLOCK),
+            saleItemsToBuy.contains(SaleItem.PLEASING_PARFAIT),
+        ).count { it }
+
+        var numHandled: Int = 0
+
+        // Create a list of components to search for only if they are items
+        // which we actually want to buy.
+        val componentsToFind: List<ComponentInterface> = saleItemsToBuy.mapNotNull {
+            when (it) {
+                SaleItem.STAR_PIECE -> LabelShopStarPiece
+                SaleItem.ALARM_CLOCK -> LabelShopAlarmClock
+                SaleItem.PLEASING_PARFAIT -> LabelShopPleasingParfait
+                else -> null
+            }
+        }
+        if (componentsToFind.isEmpty()) {
+            return false
+        }
+
+        val componentToBuy: ComponentInterface? = componentsToFind.firstOrNull {
+            it.check(game.imageUtils)
+        }
+
+        if (componentToBuy == null) {
+            return false
+        }
+
+        if (component != ButtonShopExchangeDisabled) {
+            // Don't use the passed bitmap since we want to try and click
+            // on the most updated location.
+
+            // Find the component in question on the screen then crop
+            // that entry out of the image.
+            val (newLoc, bitmap) = componentToBuy.find(game.imageUtils)
+            if (newLoc == null) {
+                return false
+            }
+            val bbox: BoundingBox = BoundingBox(
+                x = 0,
+                y = (point.y - (bitmap.height / 2)).toInt(),
+                w = (bitmap.width).toInt(),
+                h = (bitmap.height).toInt(),
+            )
+            val cropped: Bitmap? = game.imageUtils.createSafeBitmap(
+                game.imageUtils.getSourceBitmap(),
+                bbox,
+                "onListEntry",
+            )
+            if (cropped == null) {
+                return false
+            }
+
+            // Finally, click the button in this entry.
+            if (!ButtonShopExchange.click(game.imageUtils, sourceBitmap = cropped)) {
+                return false
+            }
+
+            val startTime = System.currentTimeMillis()
+            while (System.currentTimeMillis() - startTime < 5000) {
+                val dialogResult: DialogHandlerResult = handleDialogs()
+                // If the sale expired, then we need to break immediately
+                // and handle this case.
+                if (bSaleExpired) {
+                    return false
+                }
+                // If we finished purchasing this item, break to proceed
+                // to the next entry in the list.
+                if (
+                    dialogResult is DialogHandlerResult.Handled &&
+                    dialogResult.dialog.name == "exchange_complete"
+                ) {
+                    return false
+                }
+            }
+        }
+
+        // Return true if we bought everything. Stops the scroll list loop.
+        //numHandled >= componentsToFind.size
+        return false
+    }
+
     override fun start(timeoutMs: Int): Boolean {
         if (!goToStart()) {
             MessageLog.e(TAG, "Failed to go to start screen for plugin.")
@@ -123,70 +224,12 @@ class DailySale(
             return false
         }
 
-        var prevNames: Set<String> = setOf()
-
-        var numToHandle: Int = listOf<Boolean>(
-            saleItemsToBuy.contains(SaleItem.STAR_PIECE),
-            saleItemsToBuy.contains(SaleItem.STAR_PIECE),
-            saleItemsToBuy.contains(SaleItem.ALARM_CLOCK),
-            saleItemsToBuy.contains(SaleItem.PLEASING_PARFAIT),
-        ).count { it }
-
-        var numHandled: Int = 0
-
-        var bSaleExpired: Boolean = false
-        
         val entryComponents: List<ComponentInterface> = listOf(
             ButtonShopExchange,
             ButtonShopExchangeDisabled,
         )
 
-        // Create a list of components to search for only if they are items
-        // which we actually want to buy.
-        val componentsToFind: List<ComponentInterface> = saleItemsToBuy.mapNotNull {
-            when (it) {
-                SaleItem.STAR_PIECE -> LabelShopStarPiece
-                SaleItem.ALARM_CLOCK -> LabelShopAlarmClock
-                SaleItem.PLEASING_PARFAIT -> LabelShopPleasingParfait
-                else -> null
-            }
-        }
-
-        scrollList.process(entryComponents) { _, _, component, loc, bitmap ->
-            val bShouldBuyItem: Boolean = componentsToFind.any {
-                it.check(game.imageUtils, sourceBitmap = bitmap)
-            }
-
-            if (bShouldBuyItem) {
-                if (component != ButtonShopExchangeDisabled) {
-                    game.tap(loc.x, loc.y, ButtonShopExchange.template.path, taps = 3)
-
-                    val startTime = System.currentTimeMillis()
-                    while (System.currentTimeMillis() - startTime < 5000) {
-                        val dialogResult: DialogHandlerResult = handleDialogs()
-                        if (dialogResult is DialogHandlerResult.Handled) {
-                            if (dialogResult.dialog.name == "return_to_shops") {
-                                // If the sale expired, then we need to break immediately
-                                // and handle this case.
-                                bSaleExpired = true
-                                break
-                            } else if (dialogResult.dialog.name == "exchange_complete") {
-                                // If we finished purchasing this item, break to proceed
-                                // to the next entry in the list.
-                                break
-                            }
-                        }
-                    }
-                    game.wait(0.5)
-                }
-
-                numHandled++
-            }
-
-            // Return true if we bought everything. Stops the scroll list loop.
-            //numHandled >= componentsToFind.size
-            false
-        }
+        scrollList.process(entryComponents, onEntry = ::onListEntry)
 
         if (!bSaleExpired) {
             ButtonShopEndSale.click(game.imageUtils, tries = 10)
@@ -194,7 +237,10 @@ class DailySale(
             game.wait(0.5)
             game.waitForLoading()
         }
+        game.wait(0.5)
         ButtonBack.click(game.imageUtils)
+        game.wait(0.5)
+        game.waitForLoading()
         return true
     }
 }
