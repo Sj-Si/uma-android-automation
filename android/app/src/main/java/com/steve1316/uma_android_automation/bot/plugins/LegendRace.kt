6e854f14061cb0e5f47438d935d2f36291ae8d12
Sj-Si <sjw.jetty@gmail.com>
2026-03-02 16:21:12 -0500

    Add logging and improve logical flow of legend race.

--- android/app/src/main/java/com/steve1316/uma_android_automation/bot/plugins/LegendRace.kt
+++ android/app/src/main/java/com/steve1316/uma_android_automation/bot/plugins/LegendRace.kt
@@ -53,6 +53,7 @@ class LegendRace(
     private var bHasHandledSpecialMissions: Boolean = false
 
     private fun selectRace(bitmap: Bitmap? = null): Boolean {
+        MessageLog.d(TAG, "[$name] Selecting race...")
         val bitmap: Bitmap = bitmap ?: game.imageUtils.getSourceBitmap()
 
         val pillBitmap: Bitmap = IconExtraRacePill.template.getBitmap(game.imageUtils)!!
@@ -66,11 +67,12 @@ class LegendRace(
 
         if (locs.isEmpty()) {
             bIsComplete = true
-            MessageLog.w(TAG, "[LEGEND_RACE] No more races available today. Finishing up.")
+            MessageLog.w(TAG, "[$name] No more races available today. Finishing up.")
             return true
         }
 
         val loc: Point = locs.first()
+        MessageLog.d(TAG, "[$name] Selecting race at $loc.")
         game.tap(loc.x, loc.y, IconExtraRacePill.template.path)
         game.waitForLoading()
 
@@ -87,8 +89,10 @@ class LegendRace(
             "daily_sale" -> {
                 val dailySale: Plugin? = PluginFactory.create("DailySale", game, menuBar, commonDialogHandler)
                 if (dailySale == null) {
+                    MessageLog.d(TAG, "[$name] DailySale plugin returned NULL.")
                     result.dialog.close(game.imageUtils)
                 } else {
+                    MessageLog.d(TAG, "[$name] Handling Daily Sale...")
                     result.dialog.ok(game.imageUtils)
                     game.wait(0.5)
                     dailySale.start()
@@ -96,11 +100,16 @@ class LegendRace(
             }
             "items_selected" -> {
                 if (bShouldUseParfait) {
+                    MessageLog.d(TAG, "[$name] Using Pleasing Parfait...")
                     IconPleasingParfait.click(game.imageUtils)
                 }
                 result.dialog.ok(game.imageUtils)
             }
             "race_details" -> result.dialog.ok(game.imageUtils)
+            "retry_race" -> {
+                MessageLog.d(TAG, "[$name] Resuming existing race...")
+                result.dialog.ok(game.imageUtils)
+            }
             "special_missions" -> {
                 handleSpecialMissionsTabs()
                 result.dialog.close(game.imageUtils)
@@ -125,10 +134,12 @@ class LegendRace(
     }
 
     private fun handleTab(tab: ComponentInterface) {
+        MessageLog.d(TAG, "[$name] Handling tab: ${tab::class.simpleName}")
         game.wait(0.25, skipWaitingForLoading = true)
         tab.click(game.imageUtils)
         game.wait(0.25, skipWaitingForLoading = true)
         if (ButtonCollectAll.checkDisabled(game.imageUtils) == true) {
+            MessageLog.d(TAG, "[$name] Tab has no rewards to collect. Moving on...")
             return
         }
         ButtonCollectAll.click(game.imageUtils)
@@ -137,6 +148,7 @@ class LegendRace(
     }
 
     private fun handleSpecialMissionsTabs() {
+        MessageLog.d(TAG, "[$name] Handling Special Missions tabs...")
         specialMissionsTabs.forEach { handleTab(it) }
         bHasHandledSpecialMissions = true
     }
@@ -148,7 +160,8 @@ class LegendRace(
         val bitmap: Bitmap = bitmap ?: game.imageUtils.getSourceBitmap()
         when (currentPage) {
             PageLegendRaceHome -> {
-                if (ButtonSpecialMissions.click(game.imageUtils)) {
+                if (ButtonSpecialMissions.click(game.imageUtils, tries = 5)) {
+                    MessageLog.d(TAG, "[$name] PageLegendRaceHome: Handling special missions...")
                     game.wait(0.5)
                     handleDialogsUntilNoneRemain()
                 }
@@ -167,8 +180,10 @@ class LegendRace(
             }
             PageExtraRacesRacePrep -> {
                 if (ButtonViewResults.checkDisabled(game.imageUtils) == true) {
+                    MessageLog.d(TAG, "[$name] PageExtraRacesRacePrep: ViewResults is disabled. Manually racing...")
                     ButtonRaceManual.click(game.imageUtils)
                 } else {
+                    MessageLog.d(TAG, "[$name] PageExtraRacesRacePrep: Skipping race...")
                     ButtonViewResults.click(game.imageUtils)
                 }
             }
@@ -178,6 +193,7 @@ class LegendRace(
                     !ButtonNext.click(game.imageUtils, sourceBitmap = bitmap) &&
                     !ButtonRaceExclamation.click(game.imageUtils, sourceBitmap = bitmap)
                 ) {
+                    MessageLog.d(TAG, "[$name] Unknown page. Tapping...")
                     game.tap(350.0, 750.0, "ok", taps = 1)
                 }
             }
@@ -199,28 +215,45 @@ class LegendRace(
         }
 
         if (!menuBar.goToRace()) {
-            MessageLog.w(TAG, "Failed to go to menu bar's Race tab.")
+            MessageLog.w(TAG, "[$name] Failed to go to menu bar's Race tab.")
             return false
         }
         
         if (waitForButton(ButtonRaceEvents, bShouldClickButton = true) == null) {
-            MessageLog.w(TAG, "Failed to find Race Events button.")
+            MessageLog.w(TAG, "[$name] Failed to find Race Events button.")
             return false
         }
 
         if (waitForButton(ButtonLegendRace, bShouldClickButton = false) == null) {
-            MessageLog.e(TAG, "Failed to find Legend Race button. Cannot proceed.")
+            MessageLog.e(TAG, "[$name] Failed to find Legend Race button. Cannot proceed.")
             return false
         }
 
         if (ButtonLegendRace.checkDisabled(game.imageUtils) == true) {
-            MessageLog.i(TAG, "Legend Race is locked. Cannot proceed.")
+            MessageLog.i(TAG, "[$name] Legend Race is locked. Cannot proceed.")
             return false
         }
 
         if (!ButtonLegendRace.click(game.imageUtils)) {
-            MessageLog.w(TAG, "Failed to click Legend Race button.")
+            MessageLog.w(TAG, "[$name] Failed to click Legend Race button.")
             return false
+        }
+
+        // If the game closes or user backs out of a legend race after selecting a runner,
+        // then clicking the LegendRace button will ask if we want to resume the race
+        // in a RetryRace dialog. Need to handle this case.
+        game.wait(0.5, skipWaitingForLoading = true)
+        val dialogResult: DialogHandlerResult = handleDialogs()
+        if (
+            dialogResult is DialogHandlerResult.Handled &&
+            dialogResult.dialog.name == "retry_race"
+        ) {
+            // Allow game to load before progressing.
+            game.wait(0.5)
+            // If we handled the RetryRace dialog, then the game will go straight
+            // to the race prep screen. So we need to return now instead of waiting
+            // for PageLegendRaceHome.
+            return true
         }
 
         return waitForPage(PageLegendRaceHome) != null
