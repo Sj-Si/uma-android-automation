dcf40b796fbee68e5cc9b50049417683e4610e18
Sj-Si <sjw.jetty@gmail.com>
2026-03-02 19:50:04 -0500

    Fix incorrect logger tag. Remove unused page.

--- android/app/src/main/java/com/steve1316/uma_android_automation/components/Page.kt
+++ android/app/src/main/java/com/steve1316/uma_android_automation/components/Page.kt
@@ -268,7 +268,7 @@ object PageClubHome : PageInterface {
 }
 
 object PageSpecialMissions : PageInterface {
-    override val TAG: String = "[${MainActivity.loggerTag}]PageClubHome"
+    override val TAG: String = "[${MainActivity.loggerTag}]PageSpecialMissions"
     override val comparisonMode: ComponentComparisonMode = ComponentComparisonMode.AND
     override val identifyingComponents: List<ComponentInterface> = listOf(
         ButtonSpecialMissionsTabDaily,
@@ -285,26 +285,6 @@ object PageSpecialMissions : PageInterface {
         // One item will always be selected and can't be found.
         // Thus the page exists if three of the four tabs exist.
         return identifyingComponents.count { it.check(imageUtils, sourceBitmap = bitmap) } == 3
-    }
-}
-
-object PageEventMissions : PageInterface {
-    override val TAG: String = "[${MainActivity.loggerTag}]PageClubHome"
-    override val comparisonMode: ComponentComparisonMode = ComponentComparisonMode.AND
-    override val identifyingComponents: List<ComponentInterface> = listOf(
-        ButtonSpecialMissionsTabDaily,
-        ButtonSpecialMissionsTabTitles,
-        ButtonEventMissionsTabLimitedTime,
-    )
-
-    override val prevButton: ComponentInterface? = ButtonBack
-    override val nextButton: ComponentInterface? = null
-
-    override fun check(imageUtils: CustomImageUtils, bitmap: Bitmap?): Boolean {
-        val bitmap: Bitmap = bitmap ?: imageUtils.getSourceBitmap()
-        // One item will always be selected and can't be found.
-        // Thus the page exists if two of the three tabs exist.
-        return identifyingComponents.count { it.check(imageUtils, sourceBitmap = bitmap) } == 2
     }
 }
 
